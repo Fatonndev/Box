c95c22cc8c1ea29d7f93f8e0711b0f8d51d98620
KeviTV <56699208+KeviTV@users.noreply.github.com>
2020-06-01 21:13:53 +0300

    Update Version.java

--- gdx/src/ru/obvilion/box/Version.java
+++ gdx/src/ru/obvilion/box/Version.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.utils.GdxRuntimeException;
 
 public class Version {
 
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "1.9.11";
 	public static final int MAJOR;
 	public static final int MINOR;
 	public static final int REVISION;
