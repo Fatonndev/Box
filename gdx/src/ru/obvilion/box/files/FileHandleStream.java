4f6722a00ccf635298a4897e98900090fe56665e
KeviTV <56699208+KeviTV@users.noreply.github.com>
2020-06-02 19:09:16 +0300

    File

--- gdx/src/ru/obvilion/box/files/FileHandleStream.java
+++ gdx/src/ru/obvilion/box/files/FileHandleStream.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  ******************************************************************************/
 
-package com.badlogic.gdx.files;
+package ru.obvilion.box.files;
 
 import java.io.File;
 import java.io.InputStream;
