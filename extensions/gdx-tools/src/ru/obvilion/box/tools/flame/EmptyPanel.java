274c9a237fcf5a9f4052eb87a1deadb4f68b3677
KeviTV <56699208+KeviTV@users.noreply.github.com>
2020-06-02 20:33:06 +0300

    tools

---
+++ extensions/gdx-tools/src/ru/obvilion/box/tools/flame/EmptyPanel.java
@@ -0,0 +1,11 @@
+package ru.obvilion.box.tools.flame;
+
+
+/** @author Inferno */
+public class EmptyPanel extends EditorPanel {
+
+	public EmptyPanel (FlameMain particleEditor3D, String name , String desc) {
+		super(particleEditor3D, name, desc);
+		setValue(null);
+	}
+}
